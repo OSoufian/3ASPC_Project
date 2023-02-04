f166f58182b97fdc8174952772a8da07832f17c6
OSoufian <osoufian@hotmail.fr>
2023-02-04 14:27:29 +0100

    Petite heure de projet solo - Ajout d'un gitignore - Ajout de la méthode POST et de la connexion à la base de données (pas encore tester)

--- iBay/Controllers/UserController.cs
+++ iBay/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using iBay.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace iBay.Controllers
 {
@@ -7,22 +8,30 @@ namespace iBay.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-
+        private readonly MySQLConnection database;
         private readonly ILogger<UserController> _logger;
 
-        public UserController(ILogger<UserController> logger)
+        public UserController(ILogger<UserController> logger, MySQLConnection database)
         {
+            this.database = database;
             _logger = logger;
         }
 
         [HttpPost(Name = "AddUser")]
-        public IEnumerable<User> Post()
+        public IEnumerable<User> Post(User user)
         {
-            return Enumerable.Range(1, 5).Select(index => new User
-            {
-                Pseudo = "loki"
-            })
-            .ToArray();
+            //return Enumerable.Range(1, 5).Select(index => new User
+            //{
+            //    Pseudo = "loki"
+            //})
+            //.ToArray();
+            User newUser entity = new User() {
+                Email = User.Email,
+                LastName = User.LastName,
+                Username = User.Username,
+                Password = User.Password,
+                EnrollmentDate = User.EnrollmentDate
+            };
         }
         [HttpGet(Name = "GetUser")]
         public IEnumerable<User> Get()
--- iBay/MySQLConnection.cs
+++ iBay/MySQLConnection.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 
-public class MySQLConnection {
+using Microsoft.EntityFrameworkCore;
 
-	public MySQLConnection() {
-        string connectionString = null;
-        MySqlConnection cnn;
-        connectionString = "server=localhost;database=iBay;uid=root;pwd=\"\";";
-        cnn = new MySqlConnection(connectionString);
-        try {
-            cnn.Open();
-            Console.WriteLine("Connection Open ! ");
-            cnn.Close();
-        } catch (Exception ex) {
-            Console.WriteLine("Can not open connection ! ");
+public class MySQLConnection : DbContext
+{
+
+    //   Connexion à la base de données qui marche :
+    //	  public MySQLConnection()
+    //   {
+    //       string connectionString = null;
+    //       MySqlConnection cnn;
+    //       connectionString = "server=localhost;database=iBay;uid=root;pwd=\"\";";
+    //       cnn = new MySqlConnection(connectionString);
+    //       try {
+    //           cnn.Open();
+    //           Console.WriteLine("Connection Open ! ");
+    //           cnn.Close();
+    //       } catch (Exception ex) {
+    //           Console.WriteLine("Can not open connection ! ");
+    //       }
+    //   }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        if (!optionsBuilder.IsConfigured) {
+            optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=;database=iBay");
         }
-
     }
 }
